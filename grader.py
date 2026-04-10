707162596adad9316eabbc1c047b958febc39225
solreven <blomlie@proton.me>
2026-04-10 13:33:08 +0200

    initial commit and setup

---
+++ grader.py
@@ -0,0 +1,6 @@
+#1. For a grading project, we need to have students.
+#2. The students will probably be objects in an array? Those are sets and dictionaries in Python - I presume. 
+#   Not sure whether it's a set or a tuple, actually.
+#    Copilot tries to tell me it's a list. 
+#   I'm not sure, so I'm going to start with sets and see why they aren't, and then I'll learn from that.
+#3. Each student will have a name and a grade.
---
+++ readme.md
@@ -0,0 +1,2 @@
+##An extremely simple report card printer.
+###Instead of following along freeCodeCamp's tutorials, I'll just try to work my way to the solution to the task and see where we differ.
